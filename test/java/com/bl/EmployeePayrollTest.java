45a3d4b8009fe7f5616f40648d2481d2e8ff4b01
Your Name <you@example.com>
2022-03-30 23:22:29 +0530

    Connecting payroll servise database

---
+++ test/java/com/bl/EmployeePayrollTest.java
@@ -0,0 +1,19 @@
+package com.bl;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class EmployeePayrollTest 
+{
+	
+		/**
+		 * Rigorous Test :-)
+		 */
+		@Test
+		public void shouldAnswerWithTrue() {
+			assertTrue(true);
+		}
+}
+
+
